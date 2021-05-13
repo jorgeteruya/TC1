d00f966563e7533d6f8f45fbfc8f86312b438c64
jorgeteruya <40158063+jorgeteruya@users.noreply.github.com>
2021-05-12 21:18:07 -0300

    criada tela de login e tipo de cadastro

---
+++ receiturario/src/main/java/com/tcc/receiturario/receiturario/ReceiturarioApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.receiturario.receiturario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiturarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiturarioApplication.class, args);
+	}
+
+}
---
+++ receiturario/src/test/java/com/tcc/receiturario/receiturario/ReceiturarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.receiturario.receiturario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiturarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
